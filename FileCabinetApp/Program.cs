e75811b0a3657e91a5a5741670868b87bca299cd
Andrew Drablenkov <andrablenkov@gmail.com>
2020-03-14 16:01:05 +0300

    Add initial version of FileCabinetApp.

---
+++ FileCabinetApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FileCabinetApp
+{
+    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1400:AccessModifierMustBeDeclared", Justification = "Reviewed.")]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
