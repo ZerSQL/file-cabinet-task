179e9deab2603d7ffb67ccf073f6e6ae47ee6aa6
Andrew Drablenkov <andrablenkov@gmail.com>
2020-06-12 05:56:31 +0300

    Using extract method technic for validation.

--- FileCabinetApp/CustomValidator.cs
+++ FileCabinetApp/CustomValidator.cs
@@ -9,6 +9,13 @@ namespace FileCabinetApp
     /// </summary>
     public class CustomValidator : IRecordValidator
     {
+        private const int MinLength = 2;
+        private const int MaxLength = 15;
+        private const decimal MinWage = 150;
+        private const short MinHeight = 135;
+        private const short MaxHeight = 250;
+        private static readonly DateTime MinDateTime = new DateTime(1900, 1, 1);
+
         /// <summary>
         /// Метод, выполняющий измененную валидацию.
         /// </summary>
@@ -25,14 +32,59 @@ namespace FileCabinetApp
                 throw new ArgumentNullException($"Name cannot be null. Please, try again. First name is {newRecord.FirstName}, last name is {newRecord.LastName}.");
             }
 
-            if (newRecord.FirstName.Length < 2 || newRecord.FirstName.Length > 15 || newRecord.FirstName.Contains(' ', StringComparison.CurrentCulture) ||
-                newRecord.LastName.Length < 2 || newRecord.LastName.Length > 15 || newRecord.LastName.Contains(' ', StringComparison.CurrentCulture) ||
-                newRecord.DateOfBirth > DateTime.Now || newRecord.DateOfBirth < new DateTime(1900, 1, 1) ||
-                newRecord.Wage < 150 ||
-                newRecord.FavouriteNumeral < '0' || newRecord.FavouriteNumeral > '9' || newRecord.FavouriteNumeral == '4' ||
-                newRecord.Height < 135 || newRecord.Height > 250)
+            ValidateFirstName(newRecord.FirstName);
+            ValidateLastName(newRecord.LastName);
+            ValidateDateOfBirth(newRecord.DateOfBirth);
+            ValidateWage(newRecord.Wage);
+            ValidateFavouriteNumeral(newRecord.FavouriteNumeral);
+            ValidateHeight(newRecord.Height);
+        }
+
+        private static void ValidateFirstName(string firstName)
+        {
+            if (firstName.Length < MinLength || firstName.Length > MaxLength || firstName.Contains(' ', StringComparison.CurrentCulture))
             {
-                throw new ArgumentException("Error input");
+                throw new ArgumentException($"Firstname must contain more than {MinLength} and less than {MaxLength} letters.");
+            }
+        }
+
+        private static void ValidateLastName(string lastName)
+        {
+            if (lastName.Length < MinLength || lastName.Length > MaxLength || lastName.Contains(' ', StringComparison.CurrentCulture))
+            {
+                throw new ArgumentException($"Lastname must contain more than {MinLength} and less than {MaxLength} letters.");
+            }
+        }
+
+        private static void ValidateDateOfBirth(DateTime dateOfBirth)
+        {
+            if (dateOfBirth > DateTime.Now || dateOfBirth < MinDateTime)
+            {
+                throw new ArgumentException($"DateOfBirth must be less than current time and more than {MinDateTime.ToShortDateString()}.");
+            }
+        }
+
+        private static void ValidateWage(decimal wage)
+        {
+            if (wage < MinWage)
+            {
+                throw new ArgumentException($"Wage must be more than {MinWage}.");
+            }
+        }
+
+        private static void ValidateFavouriteNumeral(char numeral)
+        {
+            if (numeral < '0' || numeral > '9' || numeral == '4')
+            {
+                throw new ArgumentException($"Numeral must be between '0' and '9' and mustn't be '4'.");
+            }
+        }
+
+        private static void ValidateHeight(short height)
+        {
+            if (height < MinHeight || height > MaxHeight)
+            {
+                throw new ArgumentException($"Height must be between {MinHeight} and {MaxHeight}.");
             }
         }
     }
